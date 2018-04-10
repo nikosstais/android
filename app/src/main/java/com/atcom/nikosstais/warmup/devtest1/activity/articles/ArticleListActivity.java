41f228401de7745f8775be5be2a6eceb3e94debd
Nikos Stais <nikos.stais@atcom.gr>
2018-04-10 19:11:16 +0300

    fix for removed options->Settings

--- app/src/main/java/com/atcom/nikosstais/warmup/devtest1/activity/articles/ArticleListActivity.java
+++ app/src/main/java/com/atcom/nikosstais/warmup/devtest1/activity/articles/ArticleListActivity.java
@@ -147,9 +147,9 @@ public class ArticleListActivity extends AppCompatActivity implements Navigation
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
